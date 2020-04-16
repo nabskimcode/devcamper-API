c0e0910ca2ed308b83a5a727e4740f6d75802adf
nabskimcode <hakimbsns@gmail.com>
2020-04-16 13:46:00 +0800

    mid dev

---
+++ routes/bootcamps.js
@@ -0,0 +1,53 @@
+const express= require('express')
+const router = express.Router()
+
+
+const {getBootcamps,
+        getBootcamp,
+       createBootcamp, 
+      updateBootcamp,
+       deleteBootcamp}
+     = require('../controllers/bootcamps')
+
+
+ router.route('/')
+  .get(getBootcamps)
+  .post(createBootcamp)
+
+  router.route('/:id')
+   .get(getBootcamp)
+   .put(updateBootcamp)
+   .delete(deleteBootcamp)
+
+
+// router.get('/', (req,res) => {
+//     // res.send('Hello from the server');
+//     res.status(200).json({success: true, msg: 'Show all bootcamps'});
+// });
+
+// router.get('/:id', (req,res) => {
+//     // res.send('Hello from the server');
+//     res.status(200).json({success: true, msg: `get bootcamp ${req.params.id}`});
+// });
+
+// router.post('/', (req,res) => {
+//     // res.send('Hello from the server');
+//     res.status(200).json({success: true, msg: 'Create new bootcamp'});
+// });
+
+// router.put('/:id', (req,res) => {
+//     // res.send('Hello from the server');
+//     res.status(200).json({success: true, msg: `Update bootcamp ${req.params.id}`});
+// });
+
+// router.delete('/:id', (req,res) => {
+//     // res.send('Hello from the server');
+//     res.status(200).json({success: true, msg: `Delete bootcamp ${req.params.id}`});
+// });
+
+router.route('/')
+.get(getBootcamps)
+.post(createBootcamp)
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,10 +1,49 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const connetDB = require('./config/db');
+const errorHandler = require('./middleware/error');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
-const app = express()
+// Connect to DB
+connetDB();
+
+
+//Router files
+const bootcamps = require('./routes/bootcamps');
+
+const app = express();
+
+// body parser
+app.use(express.json());
+
+// Custom logger 
+// const logger = require('./middleware/logger');
+// app.use(logger);
+
+//Dev logging middleware
+if (process.env.NODE_ENV == 'development') {
+    app.use(morgan('dev'));
+}
+
+
+
+
+//Mount router
+app.use('/api/v1/bootcamps', bootcamps);
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+// Handle unhandle promise rejections
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    //Close server and exit process
+    server.close(() => process.exit(1));
+
+})
